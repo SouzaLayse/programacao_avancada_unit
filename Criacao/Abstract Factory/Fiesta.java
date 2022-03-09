ea14546b9fa6bdb6783c76afb5994b5eefd1c209
Layse Santos Souza <99034673+SouzaLayse@users.noreply.github.com>
2022-03-09 16:03:20 -0300

    Add files via upload

---
+++ Criacao/Abstract Factory/Fiesta.java
@@ -0,0 +1,11 @@
+
+package AbstractFactory;
+
+public class Fiesta implements CarroPopular {
+
+	@Override
+	public void exibirInfoPopular() {
+		System.out.println("Modelo: Fiesta\nFábrica: Ford\nCategoria:Popular");
+	}
+    
+}
---
+++ Criacao/Abstract Factory/FiestaSedan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AbstractFactory;
+
+/**
+ *
+ * @author layss
+ */
+public class FiestaSedan implements CarroSedan {
+
+	@Override
+	public void exibirInfoSedan() {
+		System.out.println("Modelo: Fiesta\nFábrica:Ford\nCategoria:Sedan");
+	}
+
+    
+}
