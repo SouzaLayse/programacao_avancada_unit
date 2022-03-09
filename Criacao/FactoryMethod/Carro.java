de8bb44d336ac3cca158447c5768c0e887f4851d
Layse Santos Souza <99034673+SouzaLayse@users.noreply.github.com>
2022-03-09 16:05:03 -0300

    Create Carro.java

---
+++ Criacao/FactoryMethod/Carro.java
@@ -0,0 +1,10 @@
+//interface comum para todos os carros -> manipular todos os carros mais fácil
+// ->  exibir informações sobre o carro 
+
+package FactoryMethod;
+
+public interface Carro {
+    
+    void exibirInfo();
+    
+}
